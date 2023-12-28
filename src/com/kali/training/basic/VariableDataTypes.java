1cc8df41970d1ae2cd8059f166de75acfeeda2ea
kaliraj1986 <kaliraj1986@gmail.com>
2023-12-28 06:12:48 +0530

    The variable and data types model class added

---
+++ src/com/kali/training/basic/VariableDataTypes.java
@@ -0,0 +1,17 @@
+package com.kali.training.basic;
+
+public class VariableDataTypes {
+
+	public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        char gender = 'M';
+        String name = "John";
+        byte simpleByte = 22;         // initializes a byte type variable B.
+        final double PI = 3.14159; //Constant
+        float gravity = 9.8f;
+        long sunDistance = 14960000000l;
+
+        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height + ", Gender: " + gender);
+    }
+}
